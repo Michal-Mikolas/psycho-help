05bc85d9336c19a4c1cc324b02f37c4b70a9d1f7
Michal Mikolas <nanuqcz@gmail.com>
2023-06-10 18:28:37 +0200

    Small thread draft improvement.

--- resources/views/thread.blade.php
+++ resources/views/thread.blade.php
@@ -11,6 +11,19 @@
 	#     # #    # #    #   #
 	 #####  #    # #    #   #
 	--}}
+    <div class="chat chat-end">
+        <div class="chat-image avatar">
+            <div class="w-10 rounded-full">
+                <img src="https://cdn-icons-png.flaticon.com/512/727/727399.png?w=740&t=st=1686413596~exp=1686414196~hmac=c6f884034d693a5d0261228b39ea1ede5d8e5fe54e3ca12ff817a30a1bcefd9e" />
+            </div>
+        </div>
+        <div class="chat-header">
+            {{-- Anakin --}}
+            <time class="text-xs opacity-50">12:45</time>
+        </div>
+        <div class="chat-bubble chat-bubble-info">It's over Anakin, <br/>I have the high ground.</div>
+    </div>
+
     <div class="chat chat-start">
         <div class="chat-image avatar">
             <div class="w-10 rounded-full">
@@ -18,10 +31,10 @@
             </div>
         </div>
         <div class="chat-header">
-            {{-- Obi-Wan Kenobi --}}
-            <time class="text-xs opacity-50">12:45</time>
+            Anakin
+            <time class="text-xs opacity-50">12:46</time>
         </div>
-        <div class="chat-bubble chat-bubble-success">You were the Chosen One!</div>
+        <div class="chat-bubble chat-bubble-success">You underestimate my power!</div>
     </div>
 
     <div class="chat chat-end">
@@ -32,10 +45,12 @@
         </div>
         <div class="chat-header">
             {{-- Anakin --}}
-            <time class="text-xs opacity-50">12:46</time>
+            <time class="text-xs opacity-50">12:47</time>
         </div>
-        <div class="chat-bubble chat-bubble-info">I hate you!</div>
+        <div class="chat-bubble chat-bubble-info">It was said that you would, destroy the Sith, not join them.</div>
     </div>
+
+	<div class="mt-4 text-xs opacity-50 italic">The psychologist will respond soon. Just come back here from time to time...</div>
 
     {{--
 	#     #
@@ -46,18 +61,12 @@
 	#     # #      #    # #    # #    # #    # #      #    #
 	#     # ######  ####   ####  #    #  ####  ######  ####
 	--}}
-    <div class="write">
+    <div class="write mt-8">
         <form action="{{ route('store') }}" method="POST">
             @csrf
-            <textarea name="message" placeholder="What troubles are you experiencing?"
+            <textarea name="message" placeholder="Your message..."
                 class="textarea textarea-bordered textarea-primary textarea-sm text-lg text-md w-full leading-normal"
                 rows="4"></textarea>
-
-            <input name="password" type="password" placeholder="Your secret password"
-                class="input input-sm input-bordered w-full max-w-xs" />
-            {{-- <input name="password_check" type="password" placeholder="Password again"
-				class="input input-sm input-bordered w-full max-w-xs"
-			/> --}}
 
             <div class="text-right mt-2">
                 <button class="btn btn-primary">
